8eb24cd2c17b31230539a888d973c8701cf9c382
Marianne Fritsch <marianne.fritsch@etu.minesparis.psl.eu>
2024-11-05 17:11:05 +0100

    v4 commit snake - version petit poucet

--- snake.py
+++ snake.py
@@ -1,7 +1,3 @@
-"""
-v1 : pareil mais au moins on peut sortir du programme
-avec la touche 'q', ou avec la souris en fermant la fenêtre
-"""
 
 from random import randint
 import pygame as pg
@@ -10,15 +6,29 @@ pg.init()
 screen = pg.display.set_mode((600, 600))
 clock = pg.time.Clock()
 
+# les coordonnées du corps du serpent
+snake = [(10, 15),(11, 15),(12, 15)]
+direction = (1,0)
 
 # on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
 running = True
+white = (255,255,255)
+black = (0,0,0)
+red = (255, 0, 0)
+green = (0,255,0)
+screen.fill(white)
+
 while running:
 
     clock.tick(1)
     
-    # les coordonnées du corps du serpent
-    snake = [(10, 15),(11, 15),(12, 15)]
+    #snake movement
+    first_x,first_y = snake[0]
+    direction_x,direction_y = direction
+    snake.pop()
+    snake.insert(0, (first_x + direction_x, first_y + direction_y))
+
+
 
     for i in range(0,601,40):
         for j in range(0,601,40) :
@@ -51,6 +61,10 @@ while running:
         rect = pg.Rect(m*width, n*height, width, height)
         pg.draw.rect(screen, color, rect)
 
+    # = (1,0)
+    #down = (-1,0)
+    #right = (0,1)
+    #left = (1,0)
 
 
     # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
@@ -65,6 +79,16 @@ while running:
             # si la touche est "Q" on veut quitter le programme
             if event.key == pg.K_q:
                 running = False
+            if event.key == pg.K_UP:
+                direction = (0,-1)
+            if event.key == pg.K_DOWN:
+                direction = (0,1)
+            if event.key == pg.K_RIGHT:
+                direction = (1,0)
+            if event.key == pg.K_LEFT:
+                direction = (-1,0)
+
+
 
     # xxx ici c'est discutable, car si on tape 'q'
     # on va quand même changer de couleur avant de sortir...
