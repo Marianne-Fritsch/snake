4a5bd5a69e02955d81f5ecc4da2f753d04650b83
Marianne Fritsch <marianne.fritsch@etu.minesparis.psl.eu>
2024-11-05 16:21:57 +0100

    v2 commit snake - création du damier

--- snake.py
+++ snake.py
@@ -7,14 +7,40 @@ from random import randint
 import pygame as pg
 
 pg.init()
-screen = pg.display.set_mode((400, 300))
+screen = pg.display.set_mode((600, 600))
 clock = pg.time.Clock()
+
 
 # on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
 running = True
 while running:
 
     clock.tick(1)
+    
+    for i in range(0,601,40):
+        for j in range(0,601,40) :
+            # les coordonnées de rectangle que l'on dessine
+            x = i # coordonnée x (colonnes) en pixels
+            y = j # coordonnée y (lignes) en pixels
+            width = 20 # largeur du rectangle en pixels
+            height = 20 # hauteur du rectangle en pixels
+            rect = pg.Rect(x, y, width, height)
+            # appel à la méthode draw.rect()
+            color = (255, 0, 0) # couleur rouge
+            pg.draw.rect(screen, color, rect)
+    
+    for i in range(20,601,40):
+        for j in range(20,601,40) :
+            # les coordonnées de rectangle que l'on dessine
+            x = i # coordonnée x (colonnes) en pixels
+            y = j # coordonnée y (lignes) en pixels
+            width = 20 # largeur du rectangle en pixels
+            height = 20 # hauteur du rectangle en pixels
+            rect = pg.Rect(x, y, width, height)
+            # appel à la méthode draw.rect()
+            color = (255, 0, 0) # couleur rouge
+            pg.draw.rect(screen, color, rect)
+                    
 
     # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
     # ici donc tous les évènements survenus durant la seconde précédente
@@ -32,11 +58,15 @@ while running:
     # xxx ici c'est discutable, car si on tape 'q'
     # on va quand même changer de couleur avant de sortir...
 
-    random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
-    screen.fill(random_color)
+    ##random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
+    ##screen.fill(random_color)
     pg.display.update()
 
 
 # Enfin on rajoute un appel à pg.quit()
 # Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
 pg.quit()
+
+
+
+
